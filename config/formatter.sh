55594f561996a883f844adca24e02cfa01bd3cf6
Ankur Luthra <ankur.luthra@edcast.com>
2022-02-25 00:49:19 +0530

    try 6

--- config/formatter.sh
+++ config/formatter.sh
@@ -4,10 +4,12 @@ jar_dir="$HOME/.local/share/java"
 jar_file="$jar_dir/google-java-format-$jar_version-all-deps.jar"
 java_cmd="java"
 
+echo $jar_file
 # download jar file if missing
 if [ ! -f "$jar_file" ]; then
     mkdir -p "$jar_dir"
-    wget -O "$jar_file" https://github.com/google/google-java-format/releases/download/google-java-format-$jar_version/google-java-format-$jar_version-all-deps.jar
+    # wget -O "$jar_file" https://github.com/google/google-java-format/releases/download/google-java-format-$jar_version/google-java-format-$jar_version-all-deps.jar
+    curl -L https://github.com/google/google-java-format/releases/download/google-java-format-$jar_version/google-java-format-$jar_version-all-deps.jar -o "$jar_file"
 fi
 
 # execute formatter
